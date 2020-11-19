2493cf127e49a0c0c00d16c1f0d7ee344624b43a
muhnashir <muhammadnashir9@gmail.com>
2020-11-19 17:22:57 +0700

    setting adminLTE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,33 @@
-lora1
-@foreach ($data as $key)
-    @if ($key['dev_id']=='arusip_lora1')        
-    {{ $key['payload_fields']['ax'] }}
-    @endif
-@endforeach
-<br>
-lora3
-@foreach ($data as $key)
-    @if ($key['dev_id']=='arusip_lora3')        
-    {{ $key['payload_fields']['ax'] }}
-    @endif
-@endforeach
+@extends('layouts.master')
+
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+    </section>
+
+
+    <section class="content">
+    {{-- @include('flash-message') --}}
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                                        lora1
+                    @foreach ($data as $key)
+                        @if ($key['dev_id']=='arusip_lora1')        
+                        {{ $key['payload_fields']['ax'] }}
+                        @endif
+                    @endforeach
+                    <br>
+                    lora3
+                    @foreach ($data as $key)
+                        @if ($key['dev_id']=='arusip_lora3')        
+                        {{ $key['payload_fields']['ax'] }}
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
